a404df29eb24d6097f8ef23dacb6d62524020a0d
Sergey Petrov <supudo@supudo.net>
2014-04-01 12:26:04 +0300

    URL fix

--- source/utils/AppSettings.js
+++ source/utils/AppSettings.js
@@ -7,8 +7,8 @@ enyo.application.appSettings['PrivateData'] = true;
 enyo.application.appSettings['PDEmail'] = "";
 
 // Misc
-enyo.application.appSettings['InDebug'] = true;
+enyo.application.appSettings['InDebug'] = false;
 enyo.application.appSettings['InDatabaseDebug'] = false;
 enyo.application.appSettings['WipeDatabase'] = false;
-enyo.application.appSettings['ServiceURL'] = "http://www.neolog.bg/service_json.php?action=";
+enyo.application.appSettings['ServiceURL'] = "http://neolog.eenk.com/service_json.php?action=";
 enyo.application.appSettings['Letters'] = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ю", "Я", "Други..."];
